13b5864a6504de69b65fe45bb5f50cb96d07d4f8
Ian Lieu <57820533+ielieu200@users.noreply.github.com>
2021-05-12 13:16:46 -0700

    Add files via upload
    This is our software implementation of LMS using SHAKE-256 instead of SHA-256. The hash-sigs code is borrowed from Cisco's repository: https://github.com/cisco/hash-sigs

---
+++ hash-sigs-sha3/test_hss.h
@@ -0,0 +1,22 @@
+#if !defined( TEST_HSS_H_ )
+#define TEST_HSS_H_
+
+#include <stdbool.h>
+
+extern bool test_testvector(bool fast_flag, bool quiet_flag);
+extern bool test_keygen(bool fast_flag, bool quiet_flag);
+extern bool test_load(bool fast_flag, bool quiet_flag);
+extern bool test_sign(bool fast_flag, bool quiet_flag);
+extern bool test_sign_inc(bool fast_flag, bool quiet_flag);
+extern bool test_stat(bool fast_flag, bool quiet_flag);
+extern bool test_verify(bool fast_flag, bool quiet_flag);
+extern bool test_verify_inc(bool fast_flag, bool quiet_flag);
+extern bool test_key_load(bool fast_flag, bool quiet_flag);
+extern bool test_reserve(bool fast_flag, bool quiet_flag);
+extern bool test_thread(bool fast_flag, bool quiet_flag);
+extern bool test_h25(bool fast_flag, bool quiet_flag);
+
+extern bool check_threading_on(bool fast_flag);
+extern bool check_h25(bool fast_flag);
+
+#endif /* TEST_HSS_H_ */
